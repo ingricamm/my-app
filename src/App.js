75c1da836af26af523a29e731e5fa5a79be4633a
ingricamm <ingricarolina1998@gmail.com>
2022-11-22 21:49:33 -0500

    pagina de inicio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+    
+              <nav className='navBar '>
+              <ul>
+                <li><a href='/about'> About</a></li>
+                <li><a href='/blog'> blog</a></li>
+                <li><a href='/Repository'> Repository</a></li>
+                <li><a href='/contact'> Contact</a></li>
+            </ul>
+              </nav>
+              <div className='App-header-content'>
+                <div className='Autor'>
+                  <h2>
+                    Carolina Mosquera Moreno
+                  </h2>
+                  
+                    Process Engineer/ Data Analytics
+                </div>
+              </div>
+              <div className='Photo-profile'>
+                
+              </div>
       </header>
     </div>
   );
